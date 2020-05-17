d91a4fefc88aa9755cb7baaf4e579d8f000697fd
Marek Zgraja <marek.zgraja@gmail.com>
2020-05-17 18:19:33 +0200

    pridany dependencies pro gpx a zapis do tmp souboru

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const BootBot = require('bootbot');
 const config = require('config');
+const togpx = require('togpx');
+const tmp = require('tmp');
+const fs = require('fs');
 
+//init
 var port = process.env.PORT || config.get('PORT');
 
 console.log('index.js inited');
@@ -16,8 +20,18 @@ bot.on('message', (payload, chat) => {
 	console.log(`The user said: ${text}`);
 });
 
-bot.hear(['hello', 'hi'], (payload, chat) => {
+bot.hear(['hello', 'hi', 'hey'], (payload, chat) => {
 	console.log('The user said "hello" or "hi"!');
+	chat.getUserProfile().then((user) => {
+		var answer = `Hello ${user.first_name}, how can I help you? You do not know? Then choose one of the commands below.`;
+		
+		chat.say({
+			text: answer,
+			buttons: [
+				{ type: 'postback', title: 'Weather', payload: 'HELP_WEATHER'}
+			]
+		});
+	});
 });
 
 bot.start(port);
